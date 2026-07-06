492c79292f2a3a1f2f04a42ae1012f3276639a74
Siwach Boonklun <siwach@Siwachs-MacBook-Air.local>
2026-07-06 14:05:57 +0700

    Deploy static export [skip ci]

---
+++ _next/static/chunks/app/layout-597c02e513557330.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{185:function(n,e,t){Promise.resolve().then(t.t.bind(t,5646,23)),Promise.resolve().then(t.t.bind(t,6340,23))},5646:function(){}},function(n){n.O(0,[490,340,293,528,744],function(){return n(n.s=185)}),_N_E=n.O()}]);
